b6a7fbc462b2a01fa92c54a52958d06016bda354
Irina Nekrasova <Irina.Tavantseva@gmail.com>
2025-06-24 12:32:17 +0300

    fix: change analyzer and formatter implementation according with following task steps

--- src/parsers.js
+++ src/parsers.js
@@ -5,21 +5,19 @@ import yaml from 'js-yaml'
 
 export { gendiff }
 
-function gendiff(filepath1, filepath2, formatType = 'stylish') {
+function gendiff(filepath1, filepath2) {
   const [data1, data2] = [filepath1, filepath2].map(loadData)
-  return genDataDiff(data1, data2, formatType)
+  return genDataDiff(data1, data2)
 }
 
 function getParser(extname) {
   if (extname === '.yml' || extname === '.yaml') {
     return yaml.load
   }
-  else if (extname === '.json') {
+  if (extname === '.json') {
     return JSON.parse
   }
-  else {
-    throw TypeError(`Unsupported file format ${extname}`)
-  }
+  throw TypeError(`Unsupported file format ${extname}`)
 }
 
 function loadData(filepath) {
@@ -29,175 +27,132 @@ function loadData(filepath) {
   return parse(content)
 }
 
-function getIndent(depth, shift) {
-  const defaultIndent = 4 // 4 spaces
-  const amount = defaultIndent * depth - shift
-  const indent = ' '.repeat(amount)
-  return indent
+function getIndent(depth, sign) {
+  return '    '.repeat(depth - 1) + `  ${sign || ' '} `
 }
 
-function formatTokens(tokens) {
-  const ast = tokens.map((token) => {
-    const result = token.result
-    let value = token.value
-    let shift = 0
-    let key = ''
-    let delim = ': '
-
-    if (_.isUndefined(value)) {
-      value = ''
-      delim = ''
-    }
-    else if (_.isNull(value)) {
-      value = 'null'
-    }
-    else {
-      value = _.toString(value)
-    }
-    if (result === 'added') {
-      // shift = 1 sign + 1 space
-      shift = 2
-      key = '+ '
-    }
-    else if (result === 'removed') {
-      shift = 2
-      key = '- '
-    }
-    key += token.key
-    const indent = getIndent(token.depth, shift)
-    return `${indent}${key}${delim}${value}`
-  })
-
-  return ast.join('\n')
+function formatTree(depth, path, nodes, outLines) {
+  nodes.forEach(node => formatNode(depth, path, node, outLines))
 }
 
-function traverseObject(obj, depth) { // create tokens for unchanged object
-  depth += 1
+function generateItem(depth, path, key, value, children, outLines, sign) {
+  const enableHighlighting = false
+  const RED = '\x1b[31m'
+  const GREEN = '\x1b[32m'
+  const RESET = '\x1b[0m'
+  const start = enableHighlighting
+    ? sign === '+' ? GREEN : sign === '-' ? RED : ''
+    : ''
+  const end = enableHighlighting ? sign ? RESET : '' : ''
+
+  if (children) {
+    outLines.push(start + getIndent(depth, sign) + key + ': {')
+    formatTree(depth + 1, [...path, key], children, outLines)
+    outLines.push(getIndent(depth) + '}' + end)
+  }
+  else {
+    outLines.push(start + getIndent(depth, sign) + `${key}: ${value}` + end)
+  }
+}
+
+function formatNode(depth, path, node, outLines) {
+  if (node.result === 'updated') {
+    generateItem(depth, path, node.key, node.from, node.fromChildren, outLines, '-')
+    generateItem(depth, path, node.key, node.to, node.toChildren, outLines, '+')
+  }
+  else {
+    const sign = node.result === 'added' ? '+' : node.result === 'removed' ? '-' : undefined
+    generateItem(depth, path, node.key, node.value, node.children, outLines, sign)
+  }
+}
+
+function stylishFormatter(tree) {
+  const outLines = ['{']
+  formatTree(1, [], tree, outLines)
+  outLines.push('}')
+  return outLines
+}
+
+function traverseObject(obj) {
   const keys = _.sortBy(Object.keys(obj))
-  return keys.flatMap((key) => {
+  return keys.map((key) => {
     const value = obj[key]
     if (_.isObject(value)) {
-      return createTokenHelper(depth)
-        .pushLine(key, '{')
-        .pushTraverseObj(value).pushLine('}')
-        .getTokens()
+      const children = traverseObject(value)
+      return { key, result: 'unchanged', children }
     }
-    return createTokenHelper(depth)
-      .pushLine(key, value)
-      .getTokens()
+    else {
+      return { key, value, result: 'unchanged' }
+    }
   })
 }
 
-function createTokenHelper(depth = 0) {
-  let tokens = []
-  const helper = {
-    pushAddLine: (key, value) => {
-      tokens.push({ key, value, depth, result: 'added' })
-      return helper
-    },
-    pushRemoveLine: (key, value) => {
-      tokens.push({ key, value, depth, result: 'removed' })
-      return helper
-    },
-    pushLine: (key, value = undefined) => {
-      tokens.push({ key, value, depth, result: 'unchanged' })
-      return helper
-    },
-    pushTraverseObj: (obj) => {
-      tokens = tokens.concat(traverseObject(obj, depth))
-      return helper
-    },
-    pushAnalyzedTokens: (list) => {
-      tokens = tokens.concat(list)
-      return helper
-    },
-    getTokens: () => tokens,
+function getNodeAddRemove(key, value, result) {
+  if (_.isObject(value)) {
+    const children = traverseObject(value)
+    return { key, result, children }
   }
-
-  return helper
+  return { key, value, result }
 }
 
-function genDataDiff(data1, data2, formatType) {
-  const calculateDiff = (obj1, obj2, depth = 0) => {
-    depth += 1
+// diffTree consists of array of nodes. Each node is an object
+// with following fields: { key, value, result, from, to,
+// children, fromChildren, toChildren }:
+
+//  key: <property_name>
+//  result: <property_compare_result>
+//  value: <property_value> | undefined
+//  from: <removed_property_value> | undefined
+//  to: <added_property_value> | undefined
+//  children: <nested_nodes> | undefined
+//  fromChildren: <removed_nested_nodes> | undefined
+//  toChildren: <added_nested_nodes> | undefined
+
+function genDataDiff(data1, data2) {
+  const calculateDiff = (obj1, obj2) => {
     const keys1 = Object.keys(obj1)
     const keys2 = Object.keys(obj2)
     const keys = _.sortBy(_.union(keys1, keys2))
 
-    return keys.flatMap((key) => {
+    return keys.map((key) => {
       const value1 = obj1[key]
       const value2 = obj2[key]
 
       if (!Object.hasOwn(obj1, key)) {
-        if (_.isObject(value2)) {
-          return createTokenHelper(depth)
-            .pushAddLine(key, '{').pushTraverseObj(value2).pushLine('}')
-            .getTokens()
-        }
-        else {
-          return createTokenHelper(depth)
-            .pushAddLine(key, value2)
-            .getTokens()
-        }
+        return getNodeAddRemove(key, value2, 'added')
       }
 
       if (!Object.hasOwn(obj2, key)) {
-        if (_.isObject(value1)) {
-          return createTokenHelper(depth)
-            .pushRemoveLine(key, '{').pushTraverseObj(value1).pushLine('}')
-            .getTokens()
-        }
-        else {
-          return createTokenHelper(depth)
-            .pushRemoveLine(key, value1)
-            .getTokens()
-        }
-      }
-
-      if (_.isObject(value1) && !_.isObject(value2)) {
-        return createTokenHelper(depth)
-          .pushRemoveLine(key, '{').pushTraverseObj(value1)
-          .pushLine('}').pushAddLine(key, value2)
-          .getTokens()
-      }
-
-      if (!_.isObject(value1) && _.isObject(value2)) {
-        return createTokenHelper(depth)
-          .pushRemoveLine(key, value1).pushAddLine(key, '{')
-          .pushTraverseObj(value2).pushLine('}')
-          .getTokens()
+        return getNodeAddRemove(key, value1, 'removed')
       }
 
       if (_.isObject(value1) && _.isObject(value2)) {
-        const diff = calculateDiff(value1, value2, depth)
-        return createTokenHelper(depth)
-          .pushLine(key, '{')
-          .pushAnalyzedTokens(diff)
-          .pushLine('}')
-          .getTokens()
+        const children = calculateDiff(value1, value2)
+        return { key, result: 'unchanged', children }
+      }
+
+      if (value1 === value2) {
+        return { key, value: value1, result: 'unchanged' }
+      }
+
+      if (_.isObject(value1) && !_.isObject(value2)) {
+        const children = traverseObject(value1)
+        return { key, result: 'updated', fromChildren: children, to: value2 }
+      }
+
+      if (!_.isObject(value1) && _.isObject(value2)) {
+        const children = traverseObject(value2)
+        return { key, result: 'updated', from: value1, toChildren: children }
       }
 
       if (value1 !== value2) {
-        return createTokenHelper(depth)
-          .pushRemoveLine(key, value1).pushAddLine(key, value2)
-          .getTokens()
+        return { key, result: 'updated', from: value1, to: value2 }
       }
-
-      if (value1 === value1) {
-        return createTokenHelper(depth)
-          .pushLine(key, value1)
-          .getTokens()
-      }
-
       throw new Error('Unknown parser error')
     })
   }
-  const diff = calculateDiff(data1, data2)
-  const tokens = createTokenHelper()
-    .pushLine('{')
-    .pushAnalyzedTokens(diff)
-    .pushLine('}')
-    .getTokens()
 
-  return formatTokens(tokens, formatType)
+  const diffTree = calculateDiff(data1, data2)
+  const lines = stylishFormatter(diffTree)
+  return (lines.join('\n'))
 }
